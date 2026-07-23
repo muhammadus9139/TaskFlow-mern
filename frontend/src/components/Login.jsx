f8ed404ce3eaaa263d55c0aaa1dd2628652c7c37
Muhammad Usama <muhammadus9139@gmail.com>
2026-07-23 23:15:09 +0500

    Update Vercel domain

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
 
         try {
             const response = await fetch(
-                "http://localhost:5000/api/auth/login",
+                "https://taskflow-mern-zth6.onrender.com/api/auth/login",
                 {
                     method: "POST",
                     headers: {
